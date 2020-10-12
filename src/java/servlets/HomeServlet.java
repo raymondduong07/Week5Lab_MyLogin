a3b62d19b95e3ee4dfdd72e5fc15f42744b7e698
rayfi <rayfi@DESKTOP-EUP6709.hitronhub.home>
2020-10-12 11:03:16 -0600

    Started on the project and made home and login page

---
+++ src/java/servlets/HomeServlet.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Raymond
+ */
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/home.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/home.jsp")
+                .forward(request, response);
+    }
+
+}
